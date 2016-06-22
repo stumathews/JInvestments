3d515e41aed8bcf80c4c9d35d62da6724247bb51
Stuart Mathews <stumathews@gmail.com>
2016-06-22 13:52:43 +0100

    all controllers have base functionality
    such as logging

---
+++ src/main/java/investments/BaseController.java
@@ -0,0 +1,14 @@
+package investments;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseController
+{
+    protected static Logger log;
+    
+    public BaseController()
+    {
+        log = LoggerFactory.getLogger(this.getClass());
+    }    
+}
