7ce3c44230f981d9a412ae5a380e4d2a4509a6f7
Stuart Mathews <stumathews@gmail.com>
2016-06-28 20:37:57 +0100

    tidy up

--- src/main/java/investments/Application.java
+++ src/main/java/investments/Application.java
@@ -6,18 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.io.fs.FileUtils;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.neo4j.core.GraphDatabase;
 
 
 
@@ -43,7 +35,7 @@ public class Application extends SpringBootServletInitializer
      * @throws Exception 
      */
     public static void main(String[] args) throws Exception {
-        FileUtils.deleteRecursively(new File("our.db"));
+        FileUtils.deleteRecursively(new File("our.db"));        
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/investments/ApplicationConfig.java
+++ src/main/java/investments/ApplicationConfig.java
@@ -4,7 +4,6 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
