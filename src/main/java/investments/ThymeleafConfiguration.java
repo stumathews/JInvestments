37d3d479b682361d4212e7df3fdb996d0ed308e0
Stuart Mathews <stumathews@gmail.com>
2016-07-12 20:50:55 +0100

    add thymleaf viewresolver to help static template cache reloading

---
+++ src/main/java/investments/ThymeleafConfiguration.java
@@ -0,0 +1,21 @@
+package investments;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+@Configuration
+public class ThymeleafConfiguration {  
+  @Bean
+  public ITemplateResolver defaultTemplateResolver() {
+    TemplateResolver resolver = new FileTemplateResolver();
+    resolver.setSuffix(".html");
+    resolver.setPrefix("src/main/resources/templates/");
+    resolver.setTemplateMode("HTML5");
+    resolver.setCharacterEncoding("UTF-8");
+    resolver.setCacheable(false);
+    return resolver;
+  }
+}
