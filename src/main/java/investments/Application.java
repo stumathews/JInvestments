1d59765aa10066d45a8035624f5e5927c7ef8c9e
Stuart Mathews <stumathews@gmail.com>
2016-07-17 20:13:07 +0100

    d3 hleper code

--- src/main/java/investments/Application.java
+++ src/main/java/investments/Application.java
@@ -1,19 +1,34 @@
 package investments;
 
+import investments.db.SpringDataNeo4jRepository;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import org.neo4j.io.fs.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
+@RestController("/")
 public class Application extends SpringBootServletInitializer 
 {    
+    @Autowired
+    SpringDataNeo4jRepository springData;
+    
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
@@ -23,4 +38,43 @@ public class Application extends SpringBootServletInitializer
         FileUtils.deleteRecursively(new File("our.db"));        
         SpringApplication.run(Application.class, args);
     }
+    
+    @RequestMapping("/graph")
+    public Map<String, Object> graph(@RequestParam(value = "limit",required = false) Integer limit) {
+        return makeGraph(limit == null ? 100 : limit);
+    }
+    private Map<String, Object> makeGraph(int limit) {
+        Iterator<Map<String, Object>> result = springData.graph(limit).iterator();
+        return toD3Format(result);
+    }
+    
+    private Map<String, Object> toD3Format(Iterator<Map<String, Object>> result) {
+        List<Map<String,Object>> nodes = new ArrayList<Map<String,Object>>();
+        List<Map<String,Object>> rels= new ArrayList<Map<String,Object>>();
+        int i=0;
+        while (result.hasNext()) {
+            Map<String, Object> row = result.next();
+            nodes.add(map("name",row.get("movie"),"label","movie"));
+            int target=i;
+            i++;
+            for (Object name : (Collection) row.get("cast")) {
+                Map<String, Object> actor = map("name", name,"label","actor");
+                int source = nodes.indexOf(actor);
+                if (source == -1) {
+                    nodes.add(actor);
+                    source = i++;
+                }
+                rels.add(map("source",source,"target",target));
+            }
+        }
+        return map("nodes", nodes, "links", rels);
+    }
+
+    private Map<String, Object> map(String key1, Object value1, String key2, Object value2) {
+        Map<String, Object> result = new HashMap<String,Object>(2);
+        result.put(key1,value1);
+        result.put(key2,value2);
+        return result;
+    }
+
 }
