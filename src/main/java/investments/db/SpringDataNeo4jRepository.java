45e02f348d604dfd1c4a10763be81bb8b7b451be
Stuart Mathews <stumathews@gmail.com>
2016-07-17 20:15:21 +0100

    add custom query for graph

--- src/main/java/investments/db/SpringDataNeo4jRepository.java
+++ src/main/java/investments/db/SpringDataNeo4jRepository.java
@@ -1,7 +1,11 @@
 package investments.db;
 
 import investments.db.del.Investment;
+import java.util.List;
+import java.util.Map;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
@@ -10,6 +14,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "investment", path = "investment")
 public interface SpringDataNeo4jRepository extends GraphRepository<Investment>
-{
-    Investment findByName(String name);       
+{    
+    Investment findByName(@Param("name") String name);
+    @Query("MATCH (m:Investment)-[:HAS_REGIONS]-(a:AssetRegion) RETURN m.name, collect(a.name) as cast LIMIT {limit}")    
+    List<Map<String,Object>> graph(@Param("limit") int limit);
 }
