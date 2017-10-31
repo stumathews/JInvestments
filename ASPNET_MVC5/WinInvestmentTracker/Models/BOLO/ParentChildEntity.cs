e511bf3f43b527a22fe06bcac02be5e5b23d04aa
Stuart Mathews <Stuart.Mathews@citrix.com>
2017-10-31 17:36:17 +0000

    make preliminary start for associating entities with investments

--- ASPNET_MVC5/WinInvestmentTracker/Models/BOLO/ParentChildEntity.cs
+++ ASPNET_MVC5/WinInvestmentTracker/Models/BOLO/ParentChildEntity.cs
@@ -3,10 +3,11 @@ using WinInvestmentTracker.Models.DEL.Interfaces;
 
 namespace WinInvestmentTracker.Models.BOLO
 {
-    public class ParentChildEntityWithHtmlActions<TEntity1, TEntity2> : ParentChildEntity<TEntity1, TEntity2>
+    public class ParentChildEntity<TEntity1, TEntity2>
         where TEntity1 : class, IDbInvestmentEntity
         where TEntity2 : class, IDbInvestmentEntity
     {
-        public IEnumerable<HtmlAction> HtmlActions { get; set; }
+        public IEnumerable<TEntity1> Children { get; set; }
+        public TEntity2 Parent { get; set; }
     }
 }
--- ASPNET_MVC5/WinInvestmentTracker/Models/CheckModel.cs
+++ ASPNET_MVC5/WinInvestmentTracker/Models/CheckModel.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WinInvestmentTracker.Models.DEL.Interfaces;
 
 namespace WinInvestmentTracker.Models
 {
-    public class CheckModel
+    public class CheckModel : IDbInvestmentEntity
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public bool Checked { get; set; }
+        public string Description { get; set; }
     }
 }
